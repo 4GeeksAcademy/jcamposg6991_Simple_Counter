5258add7e8fd52b71acb9475b1e092318ca3af7a
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-10-28 22:11:48 +0000

    codificacion de backEnd simple_counter.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import SimpleCounter from "./simple_counter";
 
+const timer = [{
+	digitOne:0,
+	digitTwo:0,
+	digitThree:0,
+	digitFour:0,
+	digitFive:0,
+	digitSix:0
+}
+]
+
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-		<SimpleCounter/>
+		{timer.map((atribute, index) => (<SimpleCounter key={index} timer={atribute} />))}
 		</>
 	);
 };
--- src/js/component/simple_counter.jsx
+++ src/js/component/simple_counter.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
-const SimpleCounter = ()=>{
+const SimpleCounter = (props)=>{
+
+    const { digitOne, digitTwo, digitThree, digitFour, digitFive, digitSix } = props.timer
+
     return(
         <>
         <div className="row d-flex justify-content-evenly py-4 bg-dark px-5">
             <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">
                 <i class="fa-regular fa-clock"></i>
             </div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">0</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">0</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">0</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">0</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">0</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitOne}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitTwo}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitThree}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitFour}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitFive}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitSix}</div>
         </div>
-
-
         </>
     )
 }
