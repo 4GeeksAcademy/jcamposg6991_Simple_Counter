cc4369f0e915245be2733b9d6f5551b4d930659c
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-10-29 01:54:13 +0000

    codificacion de logica de secondsCounter finalizacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SecondsCounter from "./secondsCounter";
 
-
-
-//create your first component
 const Home = () => {
+  const [seconds, setSeconds] = useState(0);
 
-	let sec = 0
 
-	setInterval(()=>{
-		const one = Math.floor((sec/1)%10)
-		console.log(one);
-		sec+=1
-	},1000)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
 
-	return (
-		<>
-		<SecondsCounter digitOne={sec} digitTwo={0} digitThree={0} digitFour={0} digitFive={0} digitSix={0}/>
-		</>
-	);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+  const digitOne = Math.floor((seconds / 1) % 10);
+  const digitTwo = Math.floor((seconds / 10) % 10);
+  const digitThree = Math.floor((seconds / 100) % 10);
+  const digitFour = Math.floor((seconds / 1000) % 10);
+  const digitFive = Math.floor((seconds / 10000) % 10);
+  const digitSix = Math.floor((seconds / 100000) % 10);
+
+  return (
+    <>
+      <SecondsCounter
+        digitOne={digitOne}
+        digitTwo={digitTwo}
+        digitThree={digitThree}
+        digitFour={digitFour}
+        digitFive={digitFive}
+        digitSix={digitSix}
+      />
+    </>
+  );
 };
 
 export default Home;
--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -10,12 +10,12 @@ const SecondsCounter = (props)=>{
             <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">
                 <i class="fa-regular fa-clock"></i>
             </div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitOne}</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitTwo}</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitThree}</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitFour}</div>
-            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitFive}</div>
             <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitSix}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitFive}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitFour}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitThree}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitTwo}</div>
+            <div className="col-1 bg-dark display-3 text-white text-center border border-2 border-secondary rounded fw-bolder">{digitOne}</div>
         </div>
         </>
     )
